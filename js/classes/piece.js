e7aaf6afb73a74d9ddb599ec31059c3ad7bed8cf
Drew-A <drew.a.anzalone@gmail.com>
2025-01-31 22:53:30 -0800

    Fixed import, changed coordinate attrs, fixed dynamic scoping bug with currying, removed unnecessary diagonal direction data from findMoves() output.

--- js/classes/piece.js
+++ js/classes/piece.js
@@ -1,14 +1,18 @@
-class Piece {
+export default class Piece {
   static diagonals = [[-1, -1], [-1, 1], [1, -1], [1, 1]] // vector ops for moving up-left, up-right, down-left, down-right
   
-  constructor(player, position, board, king = false) {
+  constructor(player, x, y, king = false) {
     this.player = player;
-    this.pos = position;
-    this.board = board;
-    this.king = false;
+    this.x = x;
+    this.y = y;
+    this.king = king;
   }
 
-  findMoves() {
+  get pos() {
+    return [this.x, this.y]
+  }
+
+  findMoves(board) {
     const potentialMoves = [];
     if (this.king) { // can move in all 4 diagonals
       potentialMoves.push([0, this.shift(0, this.pos)]); // up left
@@ -22,14 +26,15 @@ class Piece {
       potentialMoves.push([2, this.shift(2, this.pos)]); // down left
       potentialMoves.push([3, this.shift(3, this.pos)]); // down right
     }
-
-    return this.removeInvalidMoves(potentialMoves);
+    return this.removeInvalidMoves(potentialMoves, board);
   }
 
-  removeInvalidMoves(moves) {
+  removeInvalidMoves(moves, board) {
     moves = moves.filter(this.inBounds);
-    const validMoves = moves.filter(this.noOverlapPlayer);
-    const validAttacks = moves.filter(this.validAttack);
+    let validMoves = moves.filter(this.noOverlapPlayerPiece(board));
+    let validAttacks = moves.filter(this.validAttack(board));
+    validMoves = validMoves.map((move) => move[1]);
+    validAttacks = validAttacks.map((move) => move[1]);
     return [validMoves, validAttacks];
   }
 
@@ -37,31 +42,35 @@ class Piece {
     return move[1][0] >= 0 && move[1][0] < 8 && move[1][1] >= 0 && move[1][1] < 8;
   }
 
-  noOverlapPlayerPiece(move) {
-    const potentialMove = this.board[move[1][0]][move[1][1]]; 
-    return potentialMove === null;
+  noOverlapPlayerPiece(board) {
+    const curry = (move) => { // I hate it I hate it I hate it but JS is dynamically scoped for some reason so I have to curry board into the filter function
+      const potentialMove = board[move[1][0]][move[1][1]]; 
+      return potentialMove === null;
+    }
+    return curry;
   }
   
-  validAttack(move) {
-    const potentialMove = this.board[move[1][0]][move[1][1]];
-    if (potentialMove === null) { return false; }
-    if (potentialMove.player === this.player) { return false; }
-
-    const destination = this.shift(move[0], move[1])
-    return this.board[destination[0]][destination[1]] === null;
+  validAttack(board) {
+    const curry = (move) => { // I hate it I hate it I hate it but JS is dynamically scoped for some reason so I have to curry board into the filter function
+      const potentialMove = board[move[1][0]][move[1][1]];
+      if (potentialMove === null) { return false; }
+      if (potentialMove.player === this.player) { return false; }
+  
+      const destination = this.shift(move[0], move[1])
+      return board[destination[0]][destination[1]] === null;
+    }
+    return curry;
   }
 
   shift(direction, position) { // return a new position after shifting the position by diagonals[direction]
-    return [position[0]+diagonals[direction][0], position[1]+diagonals[direction][1]]
+    return [position[0]+Piece.diagonals[direction][0], position[1]+Piece.diagonals[direction][1]]
   }
 
   makeMove() {
-    this.board
+    1+1; // random line of code for unimplemented function
   }
 
   attackMove() {
-
+    5+5; // random line of code for unimplemented function
   }
 }
-
-export default Piece;
