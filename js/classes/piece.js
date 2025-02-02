4b5920965a893e22633ab9a9d4aff6534f0525a4
Drew-A <drew.a.anzalone@gmail.com>
2025-02-01 16:04:47 -0800

    Removed usage of unclear arrays in favor of objects with named attrs. Removed currying. Split some complex lines into more lines with better naming

--- js/classes/piece.js
+++ js/classes/piece.js
@@ -1,10 +1,16 @@
 export default class Piece {
-  static diagonals = [[-1, -1], [-1, 1], [1, -1], [1, 1]] // vector ops for moving up-left, up-right, down-left, down-right
-  
-  constructor(player, x, y, king = false) {
+  static diagonals = {
+    upLeft: [-1, -1],
+    upRight: [-1, 1],
+    downLeft: [1, -1],
+    downRight: [1, 1]
+  }
+
+  constructor(player, x, y, board, king = false) {
     this.player = player;
     this.x = x;
     this.y = y;
+    this.board = board;
     this.king = king;
   }
 
@@ -12,65 +18,69 @@ export default class Piece {
     return [this.x, this.y]
   }
 
-  findMoves(board) {
+  findMoves() {
     const potentialMoves = [];
     if (this.king) { // can move in all 4 diagonals
-      potentialMoves.push([0, this.shift(0, this.pos)]); // up left
-      potentialMoves.push([1, this.shift(1, this.pos)]); // up right
-      potentialMoves.push([2, this.shift(2, this.pos)]); // down left
-      potentialMoves.push([3, this.shift(3, this.pos)]); // down right
+      potentialMoves.push(this.makePotentialMove("upLeft"));
+      potentialMoves.push(this.makePotentialMove("upRight"));
+      potentialMoves.push(this.makePotentialMove("downLeft"));
+      potentialMoves.push(this.makePotentialMove("downRight"));
     } else if (this.player) { // player is black and not kinged -- moving up 
-      potentialMoves.push([0, this.shift(0, this.pos)]); // up left
-      potentialMoves.push([1, this.shift(1, this.pos)]); // up right
+      potentialMoves.push(this.makePotentialMove("upLeft"));
+      potentialMoves.push(this.makePotentialMove("upRight"));
     } else { // player is red and not kinged -- moving down
-      potentialMoves.push([2, this.shift(2, this.pos)]); // down left
-      potentialMoves.push([3, this.shift(3, this.pos)]); // down right
+      potentialMoves.push(this.makePotentialMove("downLeft"));
+      potentialMoves.push(this.makePotentialMove("downRight"));
     }
-    return this.removeInvalidMoves(potentialMoves, board);
+    return this.removeInvalidMoves(potentialMoves);
   }
 
-  removeInvalidMoves(moves, board) {
+  makePotentialMove(direction) {
+    return {
+      dir: direction,
+      newPos: this.shift(direction, this.pos)
+    };
+  }
+
+  removeInvalidMoves(moves) {
     moves = moves.filter(this.inBounds);
-    let validMoves = moves.filter(this.noOverlapPlayerPiece(board));
-    let validAttacks = moves.filter(this.validAttack(board));
-    validMoves = validMoves.map((move) => move[1]);
-    validAttacks = validAttacks.map((move) => move[1]);
+    let validMoves = moves.filter((move) => this.noOverlapPlayerPiece(move, this.board));
+    let validAttacks = moves.filter((move) => this.validAttack(move, this.board));
+    validMoves = validMoves.map((move) => move.newPos);
+    validAttacks = validAttacks.map((move) => move.newPos);
     return [validMoves, validAttacks];
   }
 
   inBounds(move) {
-    return move[1][0] >= 0 && move[1][0] < 8 && move[1][1] >= 0 && move[1][1] < 8;
+    return move.newPos[0] >= 0 && move.newPos[0] < 8 && move.newPos[1] >= 0 && move.newPos[1] < 8;
   }
 
-  noOverlapPlayerPiece(board) {
-    const curry = (move) => { // I hate it I hate it I hate it but JS is dynamically scoped for some reason so I have to curry board into the filter function
-      const potentialMove = board[move[1][0]][move[1][1]]; 
-      return potentialMove === null;
-    }
-    return curry;
-  }
-  
-  validAttack(board) {
-    const curry = (move) => { // I hate it I hate it I hate it but JS is dynamically scoped for some reason so I have to curry board into the filter function
-      const potentialMove = board[move[1][0]][move[1][1]];
-      if (potentialMove === null) { return false; }
-      if (potentialMove.player === this.player) { return false; }
-  
-      const destination = this.shift(move[0], move[1])
-      return board[destination[0]][destination[1]] === null;
-    }
-    return curry;
+  noOverlapPlayerPiece(move, board) {
+    const potentialMove = board[move.newPos[0]][move.newPos[1]];
+    return potentialMove === null;
   }
 
-  shift(direction, position) { // return a new position after shifting the position by diagonals[direction]
-    return [position[0]+Piece.diagonals[direction][0], position[1]+Piece.diagonals[direction][1]]
+  validAttack(move, board) {
+    const potentialMove = board[move.newPos[0]][move.newPos[1]];
+    if (potentialMove === null) { return false; }
+    if (potentialMove.player === this.player) { return false; }
+
+    const destination = this.shift(move.dir, move.newPos)
+    return board[destination[0]][destination[1]] === null;
+  }
+
+  shift(dir, currPos) { // return a new position after shifting the position by diagonals[direction]
+    const direction = Piece.diagonals[dir];
+    const newX = currPos[0] + direction[0];
+    const newY = currPos[1] + direction[1];
+    return [newX, newY];
   }
 
   makeMove() {
-    1+1; // random line of code for unimplemented function
+    1 + 1; // random line of code for unimplemented function
   }
 
   attackMove() {
-    5+5; // random line of code for unimplemented function
+    5 + 5; // random line of code for unimplemented function
   }
 }
