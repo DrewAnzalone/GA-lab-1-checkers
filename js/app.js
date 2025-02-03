f7ce22f0a9a9323522546867475d746b0ab1f64b
Drew-A <drew.a.anzalone@gmail.com>
2025-02-03 07:58:35 -0800

    up-to-date random testing code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
+import Piece from "./classes/piece.js";
+import Player from "./classes/player.js";
+
 const print = console.log;
-const board = Array(8).fill().map(() => Array(8).fill(null));
 let blackTurn = true;
-// print(board);
+const player1 = new Player(blackTurn);
+const player2 = new Player(!blackTurn);
+const board = Array(8).fill().map(() => Array(8).fill(null));
+player1.addPiece(1, 1, board);
+print(player1);
+const [moves, attacks] = player1.pieces[0].findMoves();
+// print(board)
+print("moves", moves)
+print("attacks", attacks)
 
 function init() {
-  
+  blackTurn = true;
+  player1.reset();
+  player2.reset();
   // reset board
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (!((i+j)&1) || (i > 2 && i < 5)) { board[i][j] = null; }
-      else { board[i][j] =  i > 3; }
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      if (!((y+x)&1) || (y > 2 && y < 5)) { board[y][x] = null; }
+      else { board[y][x] =  y > 3; }
     }
   }
 }
 
-init();
-print(board);
+// init();
+// print(board);
