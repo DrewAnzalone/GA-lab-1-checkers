adc91bcc98d1f6960db9280da8bd2ef99c3b7f78
Drew-A <drew.a.anzalone@gmail.com>
2025-02-03 07:58:19 -0800

    added reset method

--- js/classes/player.js
+++ js/classes/player.js
@@ -3,12 +3,16 @@ import Piece from "./piece.js"
 export default class Player {
   constructor(isBlack) {
     this.isBlack = isBlack;
-    this.pieces = []
+    this.pieces = [];
   }
 
   addPiece(y, x, board) {
     const piece = new Piece(this.isBlack, y, x, board);
     this.pieces.push(piece);
     return piece;
+  }
+
+  reset() {
+    this.pieces.lengh = 0;
   }
 }
