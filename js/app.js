376e95fc1325769b0b3cfb24058930ce018e1f19
Drew-A <drew.a.anzalone@gmail.com>
2025-02-03 19:24:11 -0800

    board rendering and prototype onClick logic

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 import Piece from "./classes/piece.js";
 import Player from "./classes/player.js";
 
+// element refs
+
+const boardElem = document.querySelector("#board");
+boardElem.addEventListener("click", tileOnClick);
+const turnTracker = document.querySelector("#current-turn");
+
 // global consts and lets
 const print = console.log;
 let blackTurn = true;
 const player1 = new Player(blackTurn);
 const player2 = new Player(!blackTurn);
 const board = Array(8).fill().map(() => Array(8).fill(null));
-
+const divmod8 = (num) => [Math.floor(num/8), num%8];
 // player1.addPiece(1, 1, board);
 // print(player1);
 // const [moves, attacks] = player1.pieces[0].findMoves();
@@ -16,22 +22,62 @@ const board = Array(8).fill().map(() => Array(8).fill(null));
 // print("attacks", attacks)
 
 // functions
+
+function tileOnClick(event) {
+  // unhighlight tiles
+  document.querySelector(".selected")?.classList.remove("selected");
+  document.querySelectorAll(".destination").forEach(e => e.classList.remove("destination"));
+
+  // pinpoint clicked tile and potential piece
+  const id = event.target.id;
+  const [y, x] = divmod8(id);
+  const piece = board[y][x];
+
+  
+  if (!piece || piece.player !== blackTurn) return; // end if clicked on null or wrong player's piece
+
+  const options = piece.findMoves();
+  // const destinations = options[0].concat(options[1])
+  print(options)
+
+}
+
 function init() {
+  turnTracker.innerText = "Black's Turn!";
   blackTurn = true;
   player1.reset();
   player2.reset();
   resetBoard();
+  // render();
 }
 
 function resetBoard() {
+  boardElem.innerHTML = ""; // wipe the board to start from 0 -- easier than trying to relocate existing pieces
+
   for (let y = 0; y < board.length; y++) {
     for (let x = 0; x < board[y].length; x++) {
-      if (!((y+x)&1) || (y > 2 && y < 5)) { // if white tile or middle row
+      const odd = (y+x)&1;
+
+      // create tile and coloring
+      const tileDiv = document.createElement("div");
+      tileDiv.id = 8*y+x;
+      tileDiv.classList.add("tile");
+      tileDiv.classList.add(odd ? "brown" : "white")
+
+      // place pieces
+      if (!odd || (y > 2 && y < 5)) { // if white tile or middle row
         board[y][x] = null;
-        continue;
+      } else if (y > 3) {
+        placePiece(player1, y, x);
+        tileDiv.classList.add("piece")
+        tileDiv.classList.add("black");
+      } else {
+        placePiece(player2, y, x);
+        tileDiv.classList.add("piece")
+        tileDiv.classList.add("red");
       }
-      if (y > 3) { placePiece(player1, y, x); }
-      else { placePiece(player2, y, x); }
+
+      boardElem.appendChild(tileDiv);
     }
   }
 }
@@ -48,9 +94,19 @@ function gameOver() {
   return false;
 }
 
-function main() {
-  init();
-  gameLoop();
+function render() {
+  for (let y = 0; y < 8; ++y) {
+    for (let x = 0; x < 8; ++x) {
+      1+1;
+    }
+  }
 }
 
+function main() {
+  init();
+  // gameLoop();
+}
+
+init()
+print(board)
 // main();
